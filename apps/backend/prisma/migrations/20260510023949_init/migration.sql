dfd6edbad77c5d53b40187548d50a69719ede6da
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-05-09 23:41:56 -0300

    feat: setup postgres with docker and initialize prisma orm

---
+++ apps/backend/prisma/migrations/20260510023949_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "externalId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "description" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_externalId_key" ON "Product"("externalId");
